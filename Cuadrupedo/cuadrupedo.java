da9a1ab019b4ee5be956d218ffd3106c58a213cb
Juan Carlos Quiñonez Madrid <149096035+CarlosMadrid11@users.noreply.github.com>
2024-04-30 03:49:14 +0000

    listaDoble

---
+++ Cuadrupedo/cuadrupedo.java
@@ -0,0 +1,33 @@
+package cuadrupedo;
+
+public class cuadrupedo {
+    private int idCuadrupedo;
+    private String tipo;
+    
+    public cuadrupedo(int idCuadrupedo, String tipo) {
+        this.idCuadrupedo = idCuadrupedo;
+        this.tipo = tipo;
+    }
+
+    public int getIdCuadrupedo() {
+        return idCuadrupedo;
+    }
+
+    public void setIdCuadrupedo(int idCuadrupedo) {
+        this.idCuadrupedo = idCuadrupedo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuadrupedo-->" + idCuadrupedo + ", tipo=" + tipo + "\n";
+    }
+    
+}
